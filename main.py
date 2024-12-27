5a366d62a2d5b2a555c7ad4dca3d0327345f7073
Dumitru Vlad-Mihai <vlad.liviu22@gmail.com>
2024-12-27 19:16:21 +0200

    Start-up for project

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+#Generate a random account from the game data
+
+#Fromat the account data into printable format
+
+#Ask user for a guess
+
+#Get follower value of each account
+
+#Check if the user is correct
+
+#Get feedback to user
+
+#Score tracking
+
+#Make the game repeatable
+
+#Make the account at position B to become the next account at position A
+
