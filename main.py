f8bc7a773bda3fcf93d32406b6c57f60c1024c6b
Dumitru Vlad-Mihai <vlad.liviu22@gmail.com>
2024-12-27 19:33:27 +0200

    Format data

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from game_data import data
+import random
 
+def format_data(account):
+    account_name = account["name"]
+    account_description = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_description}, from {account_country}."
 
-#Generate a random account from the game data
+account_A = random.choice(data)
+account_B = random.choice(data)
+if account_A == account_B:
+    account_B = random.choice(data)
 
-#Fromat the account data into printable format
+print(f"Compare A: {format_data(account_A)}")
+print(f"Against B: {format_data(account_B)}")
+
 
 #Ask user for a guess
 
